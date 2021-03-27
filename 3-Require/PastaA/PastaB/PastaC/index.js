addd1751ae353ba26c422a74fc33e91df9f149da
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-26 22:03:15 -0300

    Aula 03 - Acessando Módulo (Criando módulo interno)

---
+++ 3-Require/PastaA/PastaB/PastaC/index.js
@@ -0,0 +1,9 @@
+let date = new Date();
+
+let day = date.getDate();
+
+let year = date.getFullYear();
+
+module.exports = {
+  dia: `Dia ${day} de Março de ${year}, estudando para ser um ótimo programador.`,
+};
